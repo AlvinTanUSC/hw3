df221c561b46692c8c76c5c228d20aedc1d96568
Alvin Tan <tanalvin@usc.edu>
2025-03-02 23:26:53 +0000

    Submission

--- heap.h
+++ heap.h
@@ -2,7 +2,8 @@
 #define HEAP_H
 #include <functional>
 #include <stdexcept>
-
+#include <vector>
+using namespace std;
 template <typename T, typename PComparator = std::less<T> >
 class Heap
 {
@@ -61,13 +62,51 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
-
+  std::vector<T> heap;
+  PComparator comp;
+  size_t m;
 
 
 
 };
 
 // Add implementation of member functions here
+template <typename T, typename PComparator>
+Heap<T, PComparator>::Heap(int m, PComparator c){
+  this->m = m;
+  comp = c;
+}
+
+template <typename T, typename PComparator>
+Heap<T, PComparator>::~Heap(){
+
+}
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::push(const T& item){
+  heap.push_back(item);
+  int idx = size() - 1;
+  int father;
+  while(idx != 0){
+    father = (idx - 1) / m;
+    if(comp(heap[idx], heap[father])){
+      std::swap(heap[idx], heap[father]);
+    } else{
+      break;
+    }
+    idx = father;
+  }
+}
+
+template <typename T, typename PComparator>
+bool Heap<T, PComparator>::empty() const{
+  return heap.empty();
+}
+
+template <typename T, typename PComparator>
+size_t Heap<T, PComparator>::size() const{
+  return heap.size();
+}
 
 
 // We will start top() for you to handle the case of 
@@ -81,13 +120,12 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
+    throw std::underflow_error("Heap Empty");
 
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
-
-
+  return heap[0];
 
 }
 
@@ -101,8 +139,31 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
+    throw std::underflow_error("Heap Empty");
 
+  }
 
+  std::swap(heap[size() - 1], heap[0]);
+  heap.pop_back();
+  int idx = 0;
+  while(true){
+    int ex_idx;
+    if(idx * m + 1 < size())
+      ex_idx = idx * m + 1;
+    else
+      break;
+    for(size_t i = 2; i <= m; i++){
+      int n_idx = idx * m + i;
+      if(n_idx >= size())
+        break;
+      if(comp(heap[n_idx], heap[ex_idx]))
+        ex_idx = n_idx;
+    }
+    if(comp(heap[ex_idx], heap[idx]))
+      std::swap(heap[ex_idx], heap[idx]);
+    else
+      break;
+    idx = ex_idx;
   }
 
 
--- llrec.cpp
+++ llrec.cpp
@@ -1,7 +1,24 @@
-#include "llrec.h"
-
+#include "llrec.h"  
+using namespace std;
 //*********************************************
 // Provide your implementation of llpivot below
 //*********************************************
 
-
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
+  if(head == NULL){
+    smaller = NULL;
+    larger = NULL;
+    return;
+  }
+  Node* temp;
+  if(head -> val <= pivot){
+    smaller = head;
+    llpivot(head->next, temp, larger, pivot);
+    smaller->next = temp;
+  } else {
+    larger = head;
+    llpivot(head->next, smaller, temp, pivot);
+    larger->next = temp;
+  }
+  head = NULL;
+}
